876448159e58ad2d7a669bccb206766dbca03030
helloarpit27s <helloarpit27@gmail.com>
2026-09-04 23:35:26 +0530

    Add files via upload

---
+++ First01.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+
+int main() { 
+
+int a; 
+int b; 
+int sum; 
+
+printf("Enter first numbers : "); 
+scanf("%d", &a);
+
+printf("Enter second number : "); 
+scanf("%d", &b);
+
+
+sum = a + b; 
+printf("Value of sum : %d\n", sum); 
+
+return 0; 
+
+}
+
+ 
+
